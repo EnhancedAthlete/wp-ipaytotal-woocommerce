de7fcacdbab8d5ddd714db4877b79dba5d585e33
Brian Henry <BrianHenryIE@gmail.com>
2019-06-05 12:57:47 -0700

    Renamed root_dir to project_root_dir, added plugin_root_dir

--- tests/wp-mock/bootstrap.php
+++ tests/wp-mock/bootstrap.php
@@ -5,8 +5,10 @@
  * @package WP_IPayTotal_WooCommerce
  */
 
-$root_dir = dirname( dirname( dirname( __FILE__ ) ) );
-require_once $root_dir . '/vendor/autoload.php'; // Composer autoloader.
+$project_root_dir = dirname( dirname( dirname( __FILE__ ) ) );
+require_once $project_root_dir . '/vendor/autoload.php'; // Composer autoloader.
+
+$plugin_root_dir  = $project_root_dir . '/trunk';
 
 WP_Mock::bootstrap();
 
