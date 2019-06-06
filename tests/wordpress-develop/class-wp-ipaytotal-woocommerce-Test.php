56d26b0a259101e828341404c372c5c838a565d2
Brian Henry <BrianHenryIE@gmail.com>
2019-06-05 17:42:58 -0700

    load_plugin_textdomain written, set to skip

--- tests/wordpress-develop/class-wp-ipaytotal-woocommerce-Test.php
+++ tests/wordpress-develop/class-wp-ipaytotal-woocommerce-Test.php
@@ -10,8 +10,21 @@
  */
 class WP_IPayTotal_WooCommerce_Test extends WP_UnitTestCase {
 
-	public function setUp() {
-		parent::setUp();
+	/**
+	 * AFAICT, this will fail until a translation has been added.
+	 *
+	 * @see https://gist.github.com/GaryJones/c8259da3a4501fd0648f19beddce0249
+	 */
+	public function test_wowp_iptwpg_load_plugin_textdomain() {
+
+		$this->markTestSkipped( 'Needs translation.' );
+
+		global $plugin_root_dir;
+
+		$this->assertTrue( file_exists( $plugin_root_dir . '/languages/' ) );
+
+		// Seems to fail because there are no translations to load.
+		$this->assertTrue( is_textdomain_loaded( 'wp-ipaytotal-woocommerce' ) );
 
 	}
 
